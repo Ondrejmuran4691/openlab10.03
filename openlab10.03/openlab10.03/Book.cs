d096bf275b4fe42d3b4ea91386884b196231605b
Ondrejmuran4691 <58992354+Ondrejmuran4691@users.noreply.github.com>
2021-11-28 18:40:52 +0100

    create a book LOTR and give it some variables and controls variables

---
+++ openlab10.03/openlab10.03/Book.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace openlab10._03
+{
+    class Book
+    {
+        public string title;
+        public int pages;
+        public string category;
+        public string author;
+        public int releaseDate;
+
+
+        public void apages()
+        {
+            if (pages < 0)
+            {
+                pages = 1;
+            }
+
+        }
+        public void areleaseDate()
+        {
+            if (releaseDate < 1405)
+            {
+                releaseDate = -1;
+            }
+            else
+            {
+                if (releaseDate > 2021)
+                {
+                    releaseDate = -1;
+                }
+            }
+        }
+
+        public void writeall()
+        {
+
+            Console.WriteLine(title);
+            Console.WriteLine(pages);
+            Console.WriteLine(category);
+            Console.WriteLine(author);
+            Console.WriteLine(releaseDate);
+        }
+    }
+}
---
+++ openlab10.03/openlab10.03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace openlab10._03
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+                Book LOTR = new Book();
+                LOTR.title = "LOTR";
+                LOTR.author = "J. R. R. Tolkien";
+                LOTR.category = "Fantasy";
+                LOTR.pages = 9250;
+                LOTR.releaseDate = 1954;
+
+
+
+
+            
+        }
+    }
+}
